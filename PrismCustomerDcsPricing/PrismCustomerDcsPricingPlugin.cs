3303e78bab92782b3a85250c3938a465e07c8fb7
Danie-Brink-PPIT <danie@pricepointit.co.za>
2026-07-28 16:41:40 +0200

    Alpha1

--- PrismCustomerDcsPricing/PrismCustomerDcsPricingPlugin.cs
+++ PrismCustomerDcsPricing/PrismCustomerDcsPricingPlugin.cs
@@ -5,7 +5,7 @@ namespace PrismCustomerDcsPricing;
 
 public class PrismCustomerDcsPricingPlugin : IPrismPlugin
 {
-    public string Id => "CUSTDCSPRICING000001";
+    public string Id => "4J0G-5TBT-TX5T-Z0SO";
     public string Name => "Customer DCS Pricing Plugin";
 
     public void ConfigureServices(IServiceCollection services)
