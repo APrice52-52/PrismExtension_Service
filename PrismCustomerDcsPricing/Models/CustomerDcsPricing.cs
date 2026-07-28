0545da57dd96c2b73d0c54df813f29aa538d891e
Danie Brink <danie@pricepointit.co.za>
2026-07-28 16:41:09 +0200

    sids

--- PrismCustomerDcsPricing/Models/CustomerDcsPricing.cs
+++ PrismCustomerDcsPricing/Models/CustomerDcsPricing.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace PrismCustomerDcsPricing.Models;
 
 public class CustomerDcsPricing
 {
+    [JsonNumberHandling(JsonNumberHandling.WriteAsString | JsonNumberHandling.AllowReadingFromString)]
     public long DcsSid { get; set; }
     public string Level { get; set; } = string.Empty;
     public decimal Discount { get; set; }
--- PrismCustomerDcsPricing/Models/DcsInfo.cs
+++ PrismCustomerDcsPricing/Models/DcsInfo.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace PrismCustomerDcsPricing.Models;
 
 public class DcsInfo
 {
+    [JsonNumberHandling(JsonNumberHandling.WriteAsString | JsonNumberHandling.AllowReadingFromString)]
     public long DcsSid { get; set; }
     public string DcsCode { get; set; } = string.Empty;
 }
